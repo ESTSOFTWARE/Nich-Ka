bafc539611c310606a3bc46e012a98e3b9754a38
Karla Melissa Corral Zarate <233313@ids.upchiapas.edu.mx>
2026-07-14 01:12:06 -0600

    feat(mainActivity): Implemented drop-down navigation bar

--- android/app/src/main/kotlin/com/nichka/nich_ka/MainActivity.kt
+++ android/app/src/main/kotlin/com/nichka/nich_ka/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.nichka.nich_ka
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity : FlutterFragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val insetsController = WindowInsetsControllerCompat(window, window.decorView)
+        insetsController.hide(WindowInsetsCompat.Type.navigationBars())
+        insetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
